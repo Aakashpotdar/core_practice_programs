218bd54dff74b2922346ca7d5ef6dbefe2f57612
aakashpotdar <potdaraakash@gmail.com>
2021-11-02 17:45:03 +0530

    Add project files.

---
+++ core_practice_programs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace core_practice_programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter choice : 1.coin flip program 2.leap year program");
+            Console.WriteLine("3.4.5.6");
+            Console.WriteLine("7.8.9.10");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    coinFlip obj1 = new coinFlip();
+                    obj1.flipCoin();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+            }
+        }
+    }
+}
---
+++ core_practice_programs/coinFlip.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace core_practice_programs
+{
+    class coinFlip
+    {
+        public void flipCoin()
+        {
+            Console.WriteLine("enter the no. of time want to flip the coin");
+            int coinNumber= int.Parse(Console.ReadLine()),coin,head=0,tail=0;
+            Random ran = new Random();
+            for(int i = 1; i < coinNumber; i++)
+            {
+                coin = ran.Next(0, 2);
+                if (coin == 1)
+                {
+                    tail++;    
+                }
+                else
+                {
+                    head++;
+                }
+            }
+            int avrageHead = (head * 100) / coinNumber;
+            int avrageTail = (tail * 100) / coinNumber;
+            Console.WriteLine("avrage no. of time head apper is:"+avrageHead+"%");
+            Console.WriteLine("avrage no. of time tail apper is:" + avrageTail + "%");
+        }
+    }
+}
